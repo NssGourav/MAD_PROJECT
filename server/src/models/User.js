deb3951dee9e26f8a49c69438e85708d1396378a
Gourav NSS <gourav341111@gmail.com>
2025-12-01 22:28:29 +0530

    chore: add user.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -19,32 +19,51 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Password is required'],
     minlength: [6, 'Password must be at least 6 characters'],
-    select: false // Don't include password in queries by default
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ['driver', 'student'],
+    default: 'student',
+    required: true
+  },
+  currentLocation: {
+    lat: {
+      type: Number,
+      default: null
+    },
+    lng: {
+      type: Number,
+      default: null
+    },
+    updatedAt: {
+      type: Date,
+      default: null
+    }
+  },
+  // For students - assign them to a driver
+  assignedDriverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: null
   }
 }, {
   timestamps: true
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
-// Method to compare password
-userSchema.methods.comparePassword = async function(candidatePassword) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
 // Remove password from JSON output
-userSchema.methods.toJSON = function() {
+userSchema.methods.toJSON = function () {
   const userObject = this.toObject();
   delete userObject.password;
   return userObject;
